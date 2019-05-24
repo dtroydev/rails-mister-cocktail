95377b45671e3bdb16eb4a6cc08b6a3fe74d8211
Daniel Troy <daniel_j_troy@hotmail.com>
2019-05-24 10:56:10 +1000

    thecocktaildb.com seed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'cocktails#index'
+
+  resources :cocktails, only: %i[index show new create] do
+    resources :doses, only: %i[new create]
+  end
+
+  resources :ingredients, only: :index
+
+  resources :doses, only: :destroy
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,78 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+
+COCKTAIL_DB_API = 'http://www.thecocktaildb.com/api/json/v1/1/'.freeze
+
+INGREDIENTS = 'list.php?i=list'.freeze
+COCKTAILS = 'filter.php?c=Cocktail'.freeze
+COCKTAIL = 'lookup.php?i='.freeze
+
+def cdb_ingredients
+  ingredients_json = URI.open("#{COCKTAIL_DB_API}#{INGREDIENTS}").read
+  ingredients = JSON.parse(ingredients_json)['drinks']
+  converted_ingredients = ingredients.map do |ingredient|
+    { name: ingredient['strIngredient1'] }
+  end
+  converted_ingredients
+end
+
+def cdb_cocktail(cocktail_db_id)
+  cocktail_details_url = "#{COCKTAIL_DB_API}#{COCKTAIL}#{cocktail_db_id}"
+  JSON.parse(URI.open(cocktail_details_url).read)['drinks'][0]
+end
+
+def cdb_cocktails
+  cocktails_json = URI.open("#{COCKTAIL_DB_API}#{COCKTAILS}").read
+  cocktails = JSON.parse(cocktails_json)['drinks']
+  converted_cocktails = cocktails.map do |cocktail|
+    { name: cocktail['strDrink'],
+      image_url: cocktail['strDrinkThumb'],
+      instructions: cdb_cocktail(cocktail['idDrink'])['strInstructions'],
+      cocktail_db_id: cocktail['idDrink'] }
+  end
+  converted_cocktails
+end
+
+Ingredient.create(cdb_ingredients)
+Cocktail.create(cdb_cocktails)
+
+# rubocop:disable Metrics/MethodLength
+# rubocop:disable Metrics/AbcSize
+
+def cdb_cocktail_doses(cocktail_db_id, cocktail_id)
+  doses = []
+  cocktail = cdb_cocktail(cocktail_db_id)
+  15.times do |n|
+    ingredient = cocktail["strIngredient#{n + 1}"]
+
+    # ingredient can be nil
+    next if !ingredient || ingredient.empty?
+
+    measure = cocktail["strMeasure#{n + 1}"]
+    description = "#{measure}#{ingredient}".strip
+    ingredient_lookup = Ingredient.where(name: ingredient).first
+    # some cocktails have ingredients that are not in the ingredient list
+    if ingredient_lookup.nil?
+      new_ingredient = Ingredient.create(name: ingredient)
+      ingredient_id = new_ingredient.id
+    else
+      ingredient_id = ingredient_lookup.id
+    end
+
+    dose = { description: description, ingredient_id: ingredient_id,
+             cocktail_id: cocktail_id }
+
+    doses << dose
+  end
+  doses
+end
+
+# rubocop:enable Metrics/MethodLength
+# rubocop:enable Metrics/AbcSize
+
+def cdb_doses
+  Cocktail.all.each do |cocktail|
+    Dose.create(cdb_cocktail_doses(cocktail.cocktail_db_id, cocktail.id))
+  end
+end
+
+cdb_doses
